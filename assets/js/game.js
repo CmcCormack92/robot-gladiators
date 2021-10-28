17b44174f0a1c803397c28f588d36b39ac55f242
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-10-28 17:45:30 -0400

    add name variables and fight function

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+var playerName = window.prompt("What is yout robot's name?");
+
+// This creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+console.log(playerName);
+
+// fight();
+
+
+
+
+
