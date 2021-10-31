d09bdbd32637c0260ec0c18550018f2f33283db6
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-10-31 17:09:55 -0400

    randomize battles

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,44 +14,91 @@ var fightOrSkip = function () {
   if (ConfirmSkip) {
     window.alert(playerInfo.name + " has decided to skip this fight! Goodbye!");
     playerInfo.money = Math.max(0, playerInfo.money - 10);
+    console.log(playerInfo.money);
 
     return true;
   }
   return false;
 }
 
+
+
 var fight = function (enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
 
-  while (enemy.health > 0 && playerInfo.health > 0) {
-
-    if (fightOrSkip());
-    break;
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
   }
 
-  // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
-  var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
 
-  enemy.health = Math.max(0, enemy.health - damage);
-  console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-  // check enemy's health
-  if (enemy.health <= 0) {
-    window.alert(enemy.name + " has died!");
-    playerInfo.money = playerInfo.money + 20;
-  } else {
-    window.alert(enemy.name + " still has " + enemy.health + " health left.");
-  }
+      // remove enemy's health by subtracting the amount we set in the damage variable
+      enemy.health = Math.max(0, enemy.health - damage);
+      console.log(
+        playerInfo.name +
+        " attacked " +
+        enemy.name +
+        ". " +
+        enemy.name +
+        " now has " +
+        enemy.health +
+        " health remaining."
+      );
 
-  var damage = randomNumber(enemy.attack - 3, enemy.attack);
-  playerInfo.health = Math.max(0, playerInfo.health - damage);
-  console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+      // check enemy's health
+      if (enemy.health <= 0) {
+        window.alert(enemy.name + " has died!");
 
-  if (playerInfo.health <= 0) {
-    window.alert(playerInfo.name + " has died!");
-  } else {
-    window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.")
+        // award player money for winning
+        playerInfo.money = playerInfo.money + 20;
+
+        // leave while() loop since enemy is dead
+        break;
+      } else {
+        window.alert(enemy.name + " still has " + enemy.health + " health left.");
+      }
+      // player gets attacked first
+    } else {
+      var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+      // remove player's health by subtracting the amount we set in the damage variable
+      playerInfo.health = Math.max(0, playerInfo.health - damage);
+      console.log(
+        enemy.name +
+        " attacked " +
+        playerInfo.name +
+        ". " +
+        playerInfo.name +
+        " now has " +
+        playerInfo.health +
+        " health remaining."
+      );
+
+      // check player's health
+      if (playerInfo.health <= 0) {
+        window.alert(playerInfo.name + " has died!");
+        // leave while() loop if player is dead
+        break;
+      } else {
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+      }
+    }
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
+
   }
 };
+
 
 var startGame = function () {
   //Reset player stats
@@ -98,7 +145,7 @@ var endGame = function () {
 
 var shop = function () {
   var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack or LEAVE the store? Please enter one, 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE to make a choice.");
-  
+
   shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
 
